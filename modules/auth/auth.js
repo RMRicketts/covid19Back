7ae133c5681c6c266ab2a2f32bf653dbb521f114
rmricketts <robertmricketts.rmr@gmail.com>
2020-04-15 01:41:33 -0700

    rricketts

--- modules/auth/auth.js
+++ modules/auth/auth.js
@@ -1,30 +1,30 @@
-const Boom = require('@hapi/boom');
-const jwt = require('../../utils/jwt.js');
-const {covidInfo, dtbackPK} = require('../../configs/config.js');
+const Boom = require("@hapi/boom");
+const jwt = require("../../utils/jwt.js");
+const { covidInfo, dtbackPK } = require("../../configs/config.js");
 
 module.exports = {
-  assign: 'authProfile',
+  assign: "authProfile",
   method: async (req, h) => {
     let accessToken = req.headers.accesstoken;
     if (accessToken === covidInfo.token) {
-      return {authorized: true};
+      return { authorized: true };
     }
     try {
-      accessToken = req.headers.Authorization.split(' ')[0];
+      accessToken = req.headers.Authorization.split(" ")[0];
     } catch (e) {
-      return Boom.unauthorized('Invalid Token');
+      return Boom.unauthorized("Invalid Token");
     }
     let payload;
     try {
       payload = jwt.verify(payload, dtbackPK);
     } catch (e) {
-      if (e.message === 'jwt expired') {
-        let error = Boom.unathorized('Access token expired');
-        error.output.payload.code = 'ACCESS_EXPIRED';
-        return error;
+      if (e.message === "jwt expired") {
+        let error = Boom.unathorized("Access token expired");
+        error.output.payload.code = "ACCESS_EXPIRED";
+        return Boom.unauthorized("Expired Access Token");
       }
-      return Boom.unauthorized('Invalid Access Token');
+      return Boom.unauthorized("Invalid Access Token");
     }
     return payload;
-  },
+  }
 };
